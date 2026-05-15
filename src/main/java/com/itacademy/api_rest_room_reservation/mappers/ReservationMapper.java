217241592a544308e1f8022f4fd4598ccf64f333
adriaclavero2 <adriaclavero2@gmail.com>
2026-05-15 11:01:16 +0200

    feat: complete ReservationMapper with complex toEntity logic. Ref #5

--- src/main/java/com/itacademy/api_rest_room_reservation/mappers/ReservationMapper.java
+++ src/main/java/com/itacademy/api_rest_room_reservation/mappers/ReservationMapper.java
@@ -1,6 +1,10 @@
 package com.itacademy.api_rest_room_reservation.mappers;
 
 import com.itacademy.api_rest_room_reservation.entities.Reservation;
+import com.itacademy.api_rest_room_reservation.entities.Room;
+import com.itacademy.api_rest_room_reservation.entities.User;
+import com.itacademy.api_rest_room_reservation.enums.ReservationStatus;
+import com.itacademy.api_rest_room_reservation.requestDTOS.ReservationRequestDTO;
 import responseDTOS.ReservationResponseDTO;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +15,7 @@ public class ReservationMapper {
         if (reservation == null) return null;
 
         ReservationResponseDTO dto = new ReservationResponseDTO();
+
         dto.setId(reservation.getId());
         dto.setUserId(reservation.getUser().getId());
         dto.setRoomId(reservation.getRoom().getId());
@@ -19,5 +24,20 @@ public class ReservationMapper {
         dto.setStatus(reservation.getStatus());
         dto.setCreatedAt(reservation.getCreatedAt());
         return dto;
+    }
+
+    public Reservation toEntity(ReservationRequestDTO dto, User user, Room room) {
+        if (dto == null) return null;
+
+        Reservation reservation = new Reservation();
+
+        reservation.setUser(user);
+        reservation.setRoom(room);
+
+        reservation.setStartDate(dto.getStartDate());
+        reservation.setEndDate(dto.getEndDate());
+
+        reservation.setStatus(ReservationStatus.ACTIVE);
+        return reservation;
     }
 }

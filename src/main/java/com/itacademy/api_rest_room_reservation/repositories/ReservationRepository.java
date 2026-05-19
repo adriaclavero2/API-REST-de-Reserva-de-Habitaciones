8ac2082190c4dc79300e35fc7ad038bd6e49a6a9
adriaclavero2 <adriaclavero2@gmail.com>
2026-05-19 23:15:19 +0200

    fix: correct HQL query syntax, spaces and parameters in ReservationRepository

--- src/main/java/com/itacademy/api_rest_room_reservation/repositories/ReservationRepository.java
+++ src/main/java/com/itacademy/api_rest_room_reservation/repositories/ReservationRepository.java
@@ -14,19 +14,19 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     List<Reservation> findByUserId(Long userId);
 
-    @Query("SELECT COUNT(r) > 0 FROM Reservation r" +
-            "WHERE r.room.id = :roomId" +
-            "AND r.status = 'ACTIVE'" +
-            "AND r.startDate < :endDaye" +
+    @Query("SELECT COUNT(r) > 0 FROM Reservation r " +
+            "WHERE r.room.id = :roomId " +
+            "AND r.status = 'ACTIVE' " +
+            "AND r.startDate < :endDate " +
             "AND r.endDate > :startDate")
     boolean existsRoomOverlap(@Param("roomId") Long roomId,
                               @Param("startDate") LocalDate startDate,
-                              @Param("endDate") LocalDate EndDate);
+                              @Param("endDate") LocalDate endDate);
 
-    @Query("SELECT COUNT(r) > 0 FROM Reservation r" +
-            "WHERE r.user.id = :uderId" +
-            "AND r.status = 'ACTIVE'" +
-            "AND r.startDate < :endDate" +
+    @Query("SELECT COUNT(r) > 0 FROM Reservation r " +
+            "WHERE r.user.id = :userId " +
+            "AND r.status = 'ACTIVE' " +
+            "AND r.startDate < :endDate " +
             "AND r.endDate > :startDate")
     boolean existsUserOverlap(@Param("userId") Long userId,
                               @Param("startDate") LocalDate startDate,
